a20d9b146d8da32ce68039f14383830b58a10e4f
Jett-S <128433032+Jett-S@users.noreply.github.com>
2023-03-21 16:45:43 +1100

    revert

--- main.tf
+++ main.tf
@@ -31,10 +31,10 @@ module "vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
+resource "aws_instance" "blog_new" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  
+
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
@@ -46,8 +46,8 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.1"
   
-  name = "blog"
-  vpc_id = module.vpc.public_subnets[1]
+  name = "blog_new"
+  vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
